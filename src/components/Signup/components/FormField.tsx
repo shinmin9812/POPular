00deebd7f7737308808b5b1a39c38f700b38ed73
youngchae <010101chae@gmail.com>
2023-06-01 21:45:24 +0900

    feat: 회원가입페이지 닉네임 input 레이아웃 생성

--- src/components/Signup/components/FormField.tsx
+++ src/components/Signup/components/FormField.tsx
@@ -2,15 +2,21 @@ import * as React from 'react';
 import styled from 'styled-components';
 
 interface Props {
-  label: '이름' | '이메일' | '비밀번호' | '비밀번호 확인' | '전화번호';
-  type: 'name' | 'email' | 'password' | 'number';
+  label: '이름' | '닉네임' | '이메일' | '비밀번호' | '비밀번호 확인' | '전화번호';
+  name: 'name' | 'nickname' | 'email' | 'password' | 'passwordConfirm' | 'phoneNumber';
+  type: 'text' | 'email' | 'password' | 'number';
 }
 
-const FormField = ({ label, type }: Props) => {
+const FormField = ({ label, name, type }: Props) => {
   return (
     <FieldContainer>
-      <Label>{label}</Label>
-      <Input type={type} onChange={(e: React.ChangeEvent<HTMLInputElement>) => console.log(e.target.value)} />
+      <Label htmlFor={name}>{label}</Label>
+      <Input
+        type={type}
+        name={name}
+        id={name}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => console.log(e.target.value)}
+      />
     </FieldContainer>
   );
 };
@@ -24,6 +30,7 @@ const FieldContainer = styled.div`
 `;
 
 const Label = styled.label`
+  font-size: var(--font-small);
   width: 270px;
   padding-left: 8px;
   margin-bottom: 8px;
--- src/components/Signup/components/SignupForm.tsx
+++ src/components/Signup/components/SignupForm.tsx
@@ -1,20 +1,31 @@
 import styled from 'styled-components';
 import FormField from './FormField';
 
-const onSubmitHandler = () => {
-  console.log('회원가입');
-};
-
 const SignupForm = () => {
   return (
-    <FormContainer onSubmit={onSubmitHandler}>
-      <FormField label={'이름'} type={'name'}></FormField>
-      <FormField label={'이메일'} type={'email'}></FormField>
-      <FormField label={'비밀번호'} type={'password'}></FormField>
-      <FormField label={'비밀번호 확인'} type={'password'}></FormField>
-      <FormField label={'전화번호'} type={'number'}></FormField>
+    <FormContainer>
+      <FormField label={'이름'} name={'name'} type={'text'}></FormField>
+      <FieldContainer>
+        <NicknameLabel>
+          <Label htmlFor="nickname">닉네임</Label>
+          <NicknameCheckMessage>다른 유저가 사용 중인 닉네임입니다.</NicknameCheckMessage>
+        </NicknameLabel>
+        <NicknameInput>
+          <Input
+            type="text"
+            name="nickname"
+            id="nickname"
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => console.log(e.target.value)}
+          />
+          <NicknameCheckButton>중복확인</NicknameCheckButton>
+        </NicknameInput>
+      </FieldContainer>
+      <FormField label={'이메일'} name={'email'} type={'email'}></FormField>
+      <FormField label={'비밀번호'} name={'password'} type={'password'}></FormField>
+      <FormField label={'비밀번호 확인'} name={'passwordConfirm'} type={'password'}></FormField>
+      <FormField label={'전화번호'} name={'phoneNumber'} type={'number'}></FormField>
       <WarningMessage>이메일 형식이 올바르지 않습니다.</WarningMessage>
-      <Button>가입하기</Button>
+      <SignupButton type="submit">가입하기</SignupButton>
     </FormContainer>
   );
 };
@@ -28,13 +39,65 @@ const FormContainer = styled.form`
   margin-top: 40px;
 `;
 
+const FieldContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 14px;
+`;
+
+const NicknameLabel = styled.div`
+  width: 270px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Label = styled.label`
+  font-size: var(--font-small);
+  padding-left: 8px;
+  margin-bottom: 8px;
+`;
+
+const NicknameCheckMessage = styled.p`
+  font-size: var(--font-micro);
+  margin-right: 4px;
+  margin-bottom: 4px;
+  display: flex;
+  align-items: center;
+`;
+
+const NicknameInput = styled.div`
+  width: 270px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Input = styled.input`
+  width: 180px;
+  height: 30px;
+  padding: 8px;
+  box-sizing: border-box;
+  border: 1px solid var(--color-sub);
+  border-radius: var(--border-radius-input);
+  font-size: var(--font-small);
+  color: var(--color-black);
+`;
+
+const NicknameCheckButton = styled.button`
+  width: 80px;
+  height: 30px;
+  font-size: 12px;
+  color: var(--color-white);
+  background-color: var(--color-main);
+  border-radius: var(--border-radius-button);
+`;
+
 const WarningMessage = styled.p`
   font-size: var(--font-small);
   color: var(--color-red);
   padding-top: 10px;
 `;
 
-const Button = styled.button`
+const SignupButton = styled.button`
   width: 270px;
   height: 40px;
   color: var(--color-white);
