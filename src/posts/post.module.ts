a5592cdecde4a944ae4664f42c06aa4c0fefef9d
baansuk <baansuk_hwang@naver.com>
2023-06-01 13:20:05 +0900

    Feat: post 스키마 정의 및 dto 작성

---
+++ src/posts/post.module.ts
@@ -0,0 +1,13 @@
+import { Module } from "@nestjs/common";
+import { MongooseModule  } from "@nestjs/mongoose";
+import { Post, PostSchema } from "./post.schema";
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Post.name , schema: PostSchema}])
+  ],
+  controllers: [],
+  providers: [],
+
+})
+export class PostModule {}
--- src/posts/post.schema.ts
+++ src/posts/post.schema.ts
@@ -1,15 +1,50 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document, SchemaOptions } from 'mongoose';
+import { User } from 'src/users/user.schema';
 
 const options: SchemaOptions = {
 	timestamps: true,
 	collection: 'Post',
 };
 
+export enum BoardType {
+  Gather = 'gather',
+  Review = 'review',
+  Free = 'free',
+}
+
 @Schema(options)
 export class Post extends Document {
 	@Prop({ required: true })
 	title: string;
+
+	@Prop({ required: true })
+	author: User;
+
+	@Prop({ required: true, enum: BoardType })
+	board: BoardType;
+
+	@Prop({ required: true })
+	content: string;
+
+	@Prop({ required: true })
+	images?: Array<string>;
+
+	@Prop({ required: true })
+	storeId?: string;
+
+	@Prop({ required: true })
+	rating?: string;
+
+	@Prop({ required: true })
+	likes: number;
+
+	@Prop({ required: true })
+	report: number;
+
+	@Prop({ required: true })
+	comments: Comment[];
+
 }
 
 export const PostSchema = SchemaFactory.createForClass(Post);
