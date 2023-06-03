65cdc877b59807e254d0d19494210558641e3ba9
youngchae <010101chae@gmail.com>
2023-06-03 16:38:50 +0900

    게시판태그 타입 설정

--- src/components/common/Board/BoardTypeTag.tsx
+++ src/components/common/Board/BoardTypeTag.tsx
@@ -1,31 +1,39 @@
 import styled from 'styled-components';
+import { BoardTypes } from '../../../types/board';
+import * as React from 'react';
 
-const getTagColor = (type: string): string => {
-  if (type === 'free') return 'var(--color-tomato)';
-  else if (type === 'review') return 'var(--color-green)';
-  else return 'var(--color-main)';
-};
+interface boardMapType {
+  name: string;
+  color: string;
+}
 
-const getTagName = (type: string): string => {
-  if (type === 'free') return '자유게시판';
-  else if (type === 'review') return '후기게시판';
-  else return '모집게시판';
-};
+const boardMap = new Map<string, boardMapType>([
+  [BoardTypes.free, { name: '자유게시판', color: 'var(--color-tomato)' }],
+  [BoardTypes.gether, { name: '모집게시판', color: 'var(--color-main)' }],
+  [BoardTypes.review, { name: '후기게시판', color: 'var(--color-green)' }],
+]);
 
-const BoardTypeTag = ({ boardType }: { boardType: string }) => {
-  return <TagContainer style={{ backgroundColor: getTagColor(boardType) }}>{getTagName(boardType)}</TagContainer>;
-};
+const BoardTypeTag = React.memo(({ boardType }: { boardType: BoardTypes }) => {
+  const { name, color } = boardMap.get(boardType) as boardMapType;
+  return <TagContainer color={color}>{name}</TagContainer>;
+});
 
 export default BoardTypeTag;
 
-const TagContainer = styled.div`
-  color: var(--color-white);
-  font-size: var(--font-micro);
-  font-weight: var(--weight-regular);
-  width: 55px;
-  padding: 4px 0;
-  border-radius: 4px;
+const TagContainer = styled.div<{ color: string }>`
   display: flex;
   justify-content: center;
   align-items: center;
+
+  width: 55px;
+
+  padding: 4px 0;
+
+  border-radius: 4px;
+
+  color: var(--color-white);
+  font-size: var(--font-micro);
+  font-weight: var(--weight-regular);
+
+  background-color: ${(props) => props.color};
 `;
