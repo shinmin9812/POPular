6ff65dc1ccdcd0889c3c16f077f6b20d3245636d
cobocho <rlaalsrb1111@naver.com>
2023-06-03 14:36:41 +0900

    fix: 알림 모킹 핸들러 미기입 수정

--- src/mocks/handlers/index.ts
+++ src/mocks/handlers/index.ts
@@ -1,6 +1,7 @@
 import { comment } from './comment';
+import { notification } from './notifiction';
 import { post } from './post';
 import { store } from './store';
 import { user } from './user';
 
-export const handlers = [...store, ...user, ...post, ...comment, ...comment];
+export const handlers = [...store, ...user, ...post, ...comment, ...comment, ...notification];
