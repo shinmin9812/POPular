b1e31b32170b4e2acf32c30598b325a1e959e0f4
youngchae <010101chae@gmail.com>
2023-06-05 11:42:05 +0900

    feat: 로그아웃 버튼 추가

--- src/components/UserMenu/components/MenuItem.tsx
+++ src/components/UserMenu/components/MenuItem.tsx
@@ -1,31 +1,7 @@
-import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
 const MenuItem = ({ link, title }: { link: string; title: string }) => {
-  return (
-    <Item>
-      {link === 'deleteAccount' ? <DeleteAccountBtn>{title}</DeleteAccountBtn> : <Link to={link}>{title}</Link>}
-    </Item>
-  );
+  return <Link to={link}>{title}</Link>;
 };
 
 export default MenuItem;
-
-const Item = styled.div`
-  width: 300px;
-  font-size: var(--font-regular);
-  border-bottom: 0.5px solid var(--color-gray);
-  padding: 20px;
-  margin: 0;
-  & a:hover {
-    color: var(--color-main);
-  }
-`;
-
-const DeleteAccountBtn = styled.div`
-  width: fit-content;
-  cursor: pointer;
-  &:hover {
-    color: var(--color-main);
-  }
-`;
