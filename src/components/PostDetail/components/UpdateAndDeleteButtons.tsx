5d4ba1ecc0bdb650da73dfa5a7601e1528373b52
leeleeleeleejun <wnstjr6293@naver.com>
2023-06-07 10:30:36 +0900

    게시글 수정, 삭제 버튼 UI 추가

--- src/components/PostDetail/components/UpdateAndDeleteButtons.tsx
+++ src/components/PostDetail/components/UpdateAndDeleteButtons.tsx
@@ -3,9 +3,11 @@ import styled from 'styled-components';
 const Button = styled.button<{ update: boolean }>`
   color: var(--color-white);
   border-radius: 8px;
-  width: 30px;
+  width: 50px;
+  height: 30px;
   font-size: var(--font-small);
   background-color: ${(props) => (props.update ? 'var(--color-main)' : 'var(--color-gray)')};
+  margin: 5px;
 `;
 
 const ButtonWrap = styled.div`
@@ -16,8 +18,10 @@ const ButtonWrap = styled.div`
 const UpdateAndDelete = () => {
   return (
     <ButtonWrap>
-      <Button update={true} />
-      <Button update={false} />
+      <Button update={true}>글 수정</Button>
+      <Button update={false}>글 삭제</Button>
     </ButtonWrap>
   );
 };
+
+export default UpdateAndDelete;
