6f7fe00bf60d9229bff9264fd31fb3e972d5f64e
cobocho <rlaalsrb1111@naver.com>
2023-06-11 17:48:04 +0900

    fix: 제거된 strictmode 복구

--- src/main.tsx
+++ src/main.tsx
@@ -14,12 +14,12 @@ const queryClient = new QueryClient();
 // }
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  // <React.StrictMode>
-  <QueryClientProvider client={queryClient}>
-    <Provider store={store}>
-      <App />
-    </Provider>
-    <ReactQueryDevtools initialIsOpen={true} />
-  </QueryClientProvider>,
-  // </React.StrictMode>,
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+      <ReactQueryDevtools initialIsOpen={true} />
+    </QueryClientProvider>
+  </React.StrictMode>,
 );
