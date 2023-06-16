0125de524cb2a65dc2f0bba0d712e916b5a1d6cd
youngchae <010101chae@gmail.com>
2023-06-16 10:17:30 +0900

    feat: checked false 알림 존재여부에 따라 빨간점 표시

--- src/components/UserMenu/components/UserProfile.tsx
+++ src/components/UserMenu/components/UserProfile.tsx
@@ -2,16 +2,44 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import { CLIENT_PATH } from '../../../constants/path';
 import NotificationIcon from '../../common/Icons/NotificationIcon';
+import { Notification } from '../../../types/notification';
+import { useEffect, useState } from 'react';
+import callApi from '../../../utils/callApi';
+import RedDot from '../../common/Icons/RedDot';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../../store';
 
-const UserProfile = ({
-  profileImage,
-  nickname,
-  userId,
-}: {
-  profileImage: string;
-  nickname: string;
-  userId: string;
-}) => {
+const UserProfile = () => {
+  const [notificationExists, setNotificationExists] = useState(false);
+  const [profileImage, setProfileImage] = useState('');
+  const [nickname, setNickname] = useState('');
+  const [userId, setUserId] = useState('');
+
+  const userData = useSelector((state: RootState) => state.UserSlice.user);
+
+  const checkNotification = async (userId: string) => {
+    const response = await callApi('GET', `http://34.22.81.36:3000/notifications/user/${userId}`);
+    const data: Notification[] = await response.json();
+    const newNotification = data.find((item: Notification) => !item.checked);
+    if (userData?.allow_notification && newNotification) {
+      setNotificationExists(true);
+    }
+  };
+
+  useEffect(() => {
+    if (userData) {
+      setProfileImage(userData.profile);
+      setNickname(userData.nickname);
+      setUserId(userData._id);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (userId) {
+      checkNotification(userId);
+    }
+  }, [userId]);
+
   return (
     <ProfileContainer>
       <Link to={`/community/user/${userId}`}>
@@ -22,6 +50,7 @@ const UserProfile = ({
       </Link>
       <Link to={CLIENT_PATH.USER_NOTIFICATIONS} style={{ display: 'flex', alignItems: 'center' }}>
         <NotificationIcon />
+        <Dot>{notificationExists ? <RedDot /> : null}</Dot>
       </Link>
     </ProfileContainer>
   );
@@ -59,4 +88,8 @@ const ImageContainer = styled.div`
     height: 100%;
     object-fit: cover;
   }
+`;
+
+const Dot = styled.div`
+  margin-bottom: 20px;
 `;
